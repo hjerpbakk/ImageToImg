cc40ce1030e32e8aead47ac59b477ee63a210c62
Runar Ovesen Hjerpbakk <hjerpbakk@gmail.com>
2015-06-14 12:48:02 +0200

    Simplified InputImageView

--- ImageToImg/InputImageView.swift
+++ ImageToImg/InputImageView.swift
@@ -10,17 +10,10 @@ class InputImageView: NSImageView, NSDraggingDestination {
     required init?(coder: NSCoder) {
         dndImage = NSImage(named: "dnd")!
         draggedImage = NSImage(named: "dragged")!
-        
         supportedExtensions = [String]()
-        let documentTypes = NSBundle.mainBundle().infoDictionary?["CFBundleDocumentTypes"] as! NSArray
-        for docInfo in documentTypes {
-            let extensions = (docInfo as! NSDictionary)["CFBundleTypeExtensions"] as! NSArray
-            for ext in extensions {
-                supportedExtensions.append(ext as! String)
-            }
-        }
-        
         super.init(coder: coder)
+        
+        populateSupportedFileTypeList()
         registerForDraggedTypes([NSFilenamesPboardType, NSURLPboardType, NSPasteboardTypeTIFF])
         AppDelegate.imageView = self
     }
@@ -39,30 +32,41 @@ class InputImageView: NSImageView, NSDraggingDestination {
         image = dndImage
     }
     
-    override func performDragOperation(sender: NSDraggingInfo) -> Bool {
-        if let board = sender.draggingPasteboard().propertyListForType("NSFilenamesPboardType") as? NSArray {
-            if let imagePath = board[0] as? String {
-                droppedFilePath = imagePath
-                return true
+    override func draggingEnded(sender: NSDraggingInfo?) {
+        if let imagePath = getFilePath(sender) {
+            droppedFilePath = imagePath
+        } else {
+            droppedFilePath = nil
+        }
+    }
+    
+    func populateSupportedFileTypeList() {
+        let documentTypes = NSBundle.mainBundle().infoDictionary?["CFBundleDocumentTypes"] as! NSArray
+        for docInfo in documentTypes {
+            let extensions = (docInfo as! NSDictionary)["CFBundleTypeExtensions"] as! NSArray
+            for ext in extensions {
+                supportedExtensions.append(ext as! String)
+            }
+        }
+    }
+   
+    func isImage(drag: NSDraggingInfo) -> Bool {
+        if let imagePath = getFilePath(drag) {
+            if let url = NSURL(fileURLWithPath: imagePath) {
+                return contains(supportedExtensions, url.pathExtension!)
             }
         }
         
-        droppedFilePath = nil
-        return false
-    }    
-   
-    func isImage(drag: NSDraggingInfo) -> Bool {
-        if let board = drag.draggingPasteboard().propertyListForType("NSFilenamesPboardType") as? NSArray {
-            if let url = NSURL(fileURLWithPath: (board[0] as! String)) {
-                let suffix = url.pathExtension!
-                for ext in self.supportedExtensions {
-                    if ext == suffix {
-                        return true
-                    }
-                }
-            }
-        }
         return false
     }
-   
+    
+    func getFilePath(sender: NSDraggingInfo?) -> String? {
+        if let draggingInfo = sender {
+            if let board = draggingInfo.draggingPasteboard().propertyListForType("NSFilenamesPboardType") as? NSArray {
+                return board[0] as? String
+            }
+        }
+        
+        return nil
+    }
 }
