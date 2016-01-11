5bb18446dc3217ed827565ee9c6221a96a2f0d39
Runar Ovesen Hjerpbakk <hjerpbakk@gmail.com>
2016-01-11 22:47:09 +0100

    Fixed red test

--- ImageToImgTests/HTMLGeneratorTests.swift
+++ ImageToImgTests/HTMLGeneratorTests.swift
@@ -7,8 +7,8 @@ class HTMLGeneratorTests: XCTestCase {
         let htmlGenerator = HTMLGenerator()
         
         let img = htmlGenerator.imgHtml("star.png", width: 200, height: 100)
-        
-        XCTAssertEqual("<div style=\"width:100%; text-align:center\">\r\n\t<img src=\"../s/star.png\" alt=\"star\" width=\"100.0\" height=\"50.0\">\r\n</div>", img)
+
+        XCTAssertEqual("<div style=\"width:100%; text-align:center\">\r\n\t<img src=\"http://hjerpbakk.com/s/star.png\" alt=\"star\" width=\"100.0\" height=\"50.0\">\r\n</div>", img)
         
     }
 }
